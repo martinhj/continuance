052a3daf3ea0da93cc271e48a7d128afcb51cc5f
Martin Holt Juliussen <martinhj@gmail.com>
2016-05-06 23:03:55 +0200

    now recieves and sends percentage finished.

--- app.js
+++ app.js
@@ -1,35 +1,38 @@
 var io = require('socket.io')();
 io.on('connection', function(socket){
 	console.log("connection.");
+	/*
+	console.log("^^^^^^^^^^\n");
+	console.log(io);
+	console.log("^^^^^^^^^^^");
+	console.log("##########\n");
+	console.log(socket);
+	console.log("###########");
+	*/
+	console.log(socket.client.conn.id);
 
 	socket.emit('message',	{ hello: 'Connected!'});
+	socket.broadcast.emit('message', { hello: 'test'});
+	io.emit('message', { hello: 'new connection...'});
 
 	socket.on('message', function(data) {
+		console.log(socket.client.conn.id);
+		console.log(data);
+	})
+	socket.on('scroll', function(data) {
 		var documentDistance 
 			= Math.abs(data.top - data.window.height - data.position.top);
+		var percentageComplete
+			= documentDistance / data.document.height * 100;
+		socket.broadcast.emit('position', percentageComplete);
+
+		/*
 		process.stdout.write(""
 				+ " percentage complete: " 
 				+ (documentDistance / data.document.height * 100)
 				+ "\n"
 				);
-		//console.log(data);
+		*/
 	})
-	/*
-	function () { if(this[0])
-		{
-			var a,b,c={top:0,left:0}, d=this[0];
-			return "fixed"===n.css(d,"position") ? b=d.getBoundingClientRect()
-			: (a=this.offsetParent(),b=this.offset(),n.nodeName(a[0],"html") 
-				|| (c=a.offset())
-					,c.top+=n.css(a[0],"borderTopWidth",!0)
-					,c.left+=n.css(a[0],"borderLeftWidth",!0))
-			, {
-				top:b.top-c.top-n.css(d,"marginTop",!0)
-				,left:b.left-c.left-n.css(d,"marginLeft",!0)
-			}
-	}
-	}
-	*/
-
 });
 io.listen(3000);
